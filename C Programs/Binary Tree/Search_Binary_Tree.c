ce044037529c17b6064e99972f00ce3116796815
Omkaarr1 <omkargupta702@gmail.com>
2022-10-06 16:50:37 +0530

    -.

--- C Programs/Binary Tree/Search_Binary_Tree.c
+++ C Programs/Binary Tree/Search_Binary_Tree.c
@@ -1,46 +1,127 @@
-#include<stdio.h>
-#include<malloc.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node
-{
-    int data;
-    struct node *left,*right;    
+struct node {
+	int data;
+	struct node *left, *right;
 };
 
-struct node *root,*nw;
-struct node *newnode(int);
-void insert(int);
+struct node* newNode(int item)
+{
+	struct node* temp
+		= (struct node*)malloc(sizeof(struct node));
+	temp->data = item;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+void inorder(struct node* root)
+{
+	if (root != NULL) {
+		inorder(root->left);
+		printf("%d ", root->data);
+		inorder(root->right);
+	}
+}
+
+struct node* insert(struct node* node, int data)
+{
+	if (node == NULL)
+		return newNode(data);
+
+	if (data < node->data)
+		node->left = insert(node->left, data);
+	else
+		node->right = insert(node->right, data);
+	return node;
+}
+
+struct node* minValueNode(struct node* node)
+{
+	struct node* current = node;
+
+	while (current && current->left != NULL)
+	current = current->left;
+
+	return current;
+}
+
+struct node* maxValueNode(struct node* node)
+{
+	struct node* current = node;
+
+	while (current && current->right != NULL)
+	current = current->right;
+
+	return current;
+}
+
+
+struct node* deleteNode(struct node* root, int data)
+{
+	if (root == NULL)
+		return root;
+	if (data < root->data)
+		root->left = deleteNode(root->left, data);
+	else if (data > root->data)
+		root->right = deleteNode(root->right, data);
+	else {
+		if (root->left == NULL) {
+			struct node* temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL) {
+			struct node* temp = root->left;
+			free(root);
+			return temp;
+		}
+		struct node* temp = minValueNode(root->right);
+		root->data = temp->data;
+		root->right = deleteNode(root->right, temp->data);
+	}
+	return root;
+}
 
 int main()
 {
-    root->data=-1;
-    root->left=-1;
-    root->right=-1;
+	/* Let us create following BST
+			50
+		/	 \
+		30	 70
+		/ \ / \
+	20 40 60 80 */
+	struct node* root = NULL;
+	root = insert(root, 50);
+	root = insert(root, 30);
+	root = insert(root, 20);
+	root = insert(root, 40);
+	root = insert(root, 70);
+	root = insert(root, 60);
+	root = insert(root, 80);
 
-    insert(rand()*100);
-    insert(rand()*100);
-}
+	printf("Inorder traversal of the given tree \n");
+	inorder(root);
 
-void insert(int a)
-{
-    if(root->data==-1)
-    root->data=a;
-    else
-    {
-        if(root->data>a)
-        root->left->data=a;
-        else
-        root->right->data=a;    
-    }
-}
+    printf("Minimum Value --> %i\n",minValueNode(root));
+    printf("Maximum Value --> %i\n",maxValueNode(root));
 
-void inorder(struct node *root)
-{
-    if(root!=NULL)
-    {
-        inorder(root->left);
-        printf("%i\t",root->data);
-        inorder(root->right);
-    }
+	printf("\nDelete 20\n");
+	root = deleteNode(root, 20);
+	printf("Inorder traversal of the modified tree \n");
+	inorder(root);
+
+	printf("\nDelete 30\n");
+	root = deleteNode(root, 30);
+	printf("Inorder traversal of the modified tree \n");
+	inorder(root);
+
+	printf("\nDelete 50\n");
+	root = deleteNode(root, 50);
+	printf("Inorder traversal of the modified tree \n");
+	inorder(root);
+
+
+
+	return 0;
 }
